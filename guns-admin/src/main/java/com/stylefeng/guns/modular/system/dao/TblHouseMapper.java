e22d061924001e4fc159061a20b780b2538d81ae
EnochStar <1181591005@zust.edu.cn>
2021-02-04 22:10:44 +0800

    Initial commit

---
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/system/dao/TblHouseMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.modular.system.dao;
+
+import com.stylefeng.guns.modular.system.model.TblHouse;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author EnochStar
+ * @since 2021-02-04
+ */
+public interface TblHouseMapper extends BaseMapper<TblHouse> {
+
+}
